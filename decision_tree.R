eb69dbaa4563a7bee52ae4b4e0000531a00df0d4
Bellino Jullian <beljul@localhost.localdomain>
2015-06-07 17:50:29 +0200

    Fit decision tree model using Giny and Entropy.

---
+++ decision_tree.R
@@ -0,0 +1,26 @@
+library(rpart)
+# Get classes from training set
+y <- training_set[, 42]
+# Get attributes from training set
+xDataFrame = data.frame(training_set[, 0:41])
+# Add columns names
+colnames(xDataFrame) <- attributes_names
+# Compute formula for decision tree
+(fmla <- as.formula(paste("y ~ ", paste(attributes_names, collapse= "+"))))
+# Compute the decision tree itself
+mytree <- rpart(fmla, data=xDataFrame, control=rpart.control(minsplit=1, cp=0), parms=list(split='information'), method="class")
+
+# Get classes from testing set
+y_test <- testing_set[, 42]
+# Get attributes from testing set
+testDataFrame = data.frame(testing_set[, 0:41])
+# Add columns names
+colnames(testDataFrame) <- attributes_names
+
+# Compute the prediction from previous decision tree
+pred <- predict(mytree, newdat=testDataFrame, type='vector')
+# Print the error rate
+# Gini : 6.58%
+# Entropy : 6.38%
+error_test = sum(as.character(y_test) != class_names[pred])
+print(error_test/length(y_test)*100)
--- import.R
+++ import.R
@@ -1,3 +1,14 @@
-# Get csv training file into a data frame variable.
+# Get csv files into a data frames.
 training_set <- read.csv("data/census_income_learn.csv", header=FALSE, quote="")
 testing_set <- read.csv("data/census_income_test.csv", header=FALSE, quote="")
+attributes_names <- c('Age', 'Worker.class', 'Industry.code', 'Occupation.code',
+                      'Education', 'Wage', 'Enroll.education', 'Marital.status',
+                      'MajorIndustry.code', 'MajorOccupation.code', 'Race', 'Hispanic.origin',
+                      'Sex', 'LaborUnion', 'Unemployment.reason', 'Employment.stat',
+                      'Capital.gains', 'Capital.losses', 'Dividends', 'Tax', 'Residence.region',
+                      'Residence.state', 'Family.stat', 'Household.summary', 'Instance.weight',
+                      'Migration.codeInmsa', 'Migration.codeInreg', 'Migration.codeWithinreg',
+                      'LiveSince1Year', 'Migration.prevSunbelt', 'Persons.worked', 'FamilyUnder18',
+                      'Father.country', 'Mother.coutry', 'Self.country', 'Citizenship', 'Business.types',
+                      'FilQuestionnaire', 'VeteransBenefits', 'Weeks.workedPerYear', 'Year')
+class_names <- c(' - 50000.', ' 50000+.')
